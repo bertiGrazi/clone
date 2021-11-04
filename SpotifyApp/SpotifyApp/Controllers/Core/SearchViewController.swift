a036234c517316f83573bd8d3a3492bd1f19bf65
Grazielli Lima Berti <70613625+bertiGrazi@users.noreply.github.com>
2021-11-04 16:55:19 -0300

    create tabBar

--- SpotifyApp/SpotifyApp/Controllers/Core/SearchViewController.swift
+++ SpotifyApp/SpotifyApp/Controllers/Core/SearchViewController.swift
@@ -5,4 +5,13 @@
 //  Created by Grazielli Berti on 04/11/21.
 //
 
-import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
+
